649ff5f1ffd6999f5dd03bac8b734ed481302c99
MGuzmanS <mags2080@gmail.com>
2016-02-19 11:47:26 -0600

    Added files via upload

---
+++ Ejercicio.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Martin.cpp
+// Author      : Martín
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//Metodo saludar
+void saludar(char* nombre) {
+	cout << "!!!Hola!!!" << nombre << endl; // prints !!!Sigan Instrucciones!!!
+}
+
+// Metodo principal
+int main() {
+	saludar("Martin Guzman");
+	return 0;
+
+}
